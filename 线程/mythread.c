8fa12ca3e442fe7c8afa99993d092a9791850be2
THEBO666 <llb333666999@163.com>
2024-09-20 19:46:04 +0800

    commit

--- 线程/mythread.c
+++ 线程/mythread.c
@@ -2,24 +2,32 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <pthread.h>
+#include <stdlib.h>
+
 
 void* Routine(void *args)
 {
-    char *msg=(char*)args;
-    while(1)
-    {
-        printf("I am %s, pid: %d, ppid: %d\n",msg,getpid(),getppid());
+    
+    int *x=(int*)args;
+    //while(1)
+    //{
+        printf("I am thread1, pid: %d, ppid: %d, x=%d\n",getpid(),getppid(),++(*x));
         sleep(1);
-    }
+    //}
 }
+
 int main()
 {
+    int *x= (int*)malloc(sizeof(int));
     pthread_t tid;
-    pthread_create(&tid,NULL,Routine,(void*)"thread1");
-    while(1)
-    {
-        printf("I am main thread, pid: %d, ppid: %d\n",getpid(),getppid());
+    pthread_create(&tid,NULL,Routine,(void*)x);
+    
+    //while(1)
+    //{
+        (*x)++;
+        printf("I am main thread, pid: %d, ppid: %d, x=%d\n",getpid(),getppid(),*x);
         sleep(5);
-    }
+    //}
+    printf("%d\n",pthread_join(tid,NULL));
     return 0;
 }
